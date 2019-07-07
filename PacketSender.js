aa641e64cf1592fd68c8841001133f9afbe99fe8
Rafał Matysiak <Kotecek3@gmail.com>
2019-07-07 23:28:41 +0200

    some minor changes, fix mod running when user just opened parcel

---
+++ PacketSender.js
@@ -0,0 +1,65 @@
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+module.exports = class PacketSender {
+    responsePackets = {
+        C_CANCEL_CONTRACT: 'S_CANCEL_CONTRACT',
+        C_REQUEST_CONTRACT: 'S_REPLY_REQUEST_CONTRACT',
+        C_SHOW_PARCEL_MESSAGE: 'S_PARCEL_READ_RECV_STATUS',
+        C_RECV_PARCEL: 'S_RECV_PARCEL',
+        C_DELETE_PARCEL: 'S_DELETE_PARCEL',
+    };
+
+    constructor(mod) {
+        this.mod = mod;
+    }
+
+    checkMail(pageIndex = 0) {
+        this.mod.send('C_LIST_PARCEL', 2, {
+            unk1: 0,
+            page: pageIndex,
+            filter: 0,
+        });
+    }
+
+    async deleteParcelMessages(contractId, messages) {
+        await this.sendPacket('C_DELETE_PARCEL', 2, {
+            id: contractId,
+            messages,
+        });
+        return sleep(100);
+    }
+
+    async claimParcelItems({id}) {
+        await this.sendPacket('C_SHOW_PARCEL_MESSAGE', 1, {id});
+        await sleep(100);
+        return this.sendPacket('C_RECV_PARCEL', 1, {id});
+    }
+
+    async cancelContract(contractId) {
+        await this.sendPacket('C_CANCEL_CONTRACT', 1, {type: 8, id: contractId});
+        return sleep(200);
+    }
+
+    async requestContract() {
+        return this.sendPacket('C_REQUEST_CONTRACT', 1, {type: 8});
+    }
+
+    async sendPacket(packet, version, payload) {
+        this.mod.send(packet, version, payload);
+
+        const responsePacket = this.responsePackets[packet];
+        if (!responsePacket) return;
+
+        return this.awaitResult(responsePacket).catch(err => console.error(err.messages));
+    }
+
+    async awaitResult(packet, timeoutTime = 10 * 1000) {
+        const successPromise = new Promise(resolve => this.mod.hookOnce(packet, 'raw', resolve));
+
+        const timeoutPromise = new Promise((_, reject) => {
+            setTimeout(reject, timeoutTime, new Error(`${packet} timeout`));
+        });
+
+        return Promise.race([successPromise, timeoutPromise]);
+    }
+};
--- index.js
+++ index.js
@@ -1,139 +1,87 @@
+const PacketSender = require('./PacketSender');
+
 module.exports = function mail(mod) {
-    let contract = -1
-    let parcels = []
-    let objParcels = {}
-    let working = false
-    let deleteAfter = false
+    let contractId = -1;
+    let parcels = [];
+    let deleteAfter = false;
+    let gatheringParcels = false;
+
+    const sender = new PacketSender(mod);
 
     mod.command.add(['mail', 'parcel'], {
         claim: () => {
-            checkMail()
+            gatheringParcels = true;
+            sender.checkMail();
         },
         readdel: () => {
-            deleteAfter = !deleteAfter
-            mod.command.message(`Deleting parcels after reading them: ${deleteAfter ? 'enabled' : 'disabled'}`)
-        }
-    })
+            deleteAfter = !deleteAfter;
+            mod.command.message(`Deleting parcels after reading them: ${deleteAfter ? 'enabled' : 'disabled'}`);
+        },
+    });
 
-    mod.hook('S_REQUEST_CONTRACT', 1, (event) => {
-        contract = event.id
-    })
-    mod.hook('S_ACCEPT_CONTRACT', 'raw', (event) => {
-        contract = -1
-        parcels = []
-    })
-    mod.hook('S_REJECT_CONTRACT', 'raw', (event) => {
-        contract = -1
-        parcels = []
-    })
-    mod.hook('S_CANCEL_CONTRACT', 'raw', (event) => {
-        contract = -1
-        parcels = []
-    })
+    const clearState = () => {
+        contractId = -1;
+        parcels = [];
+    };
+    mod.hook('S_ACCEPT_CONTRACT', 'raw', clearState);
+    mod.hook('S_REJECT_CONTRACT', 'raw', clearState);
+    mod.hook('S_CANCEL_CONTRACT', 'raw', clearState);
+    mod.hook('S_REQUEST_CONTRACT', 1, event => {
+        contractId = event.id;
+    });
 
-    mod.hook('S_LIST_PARCEL_EX', 2, (event) => {
-        for (let message of event.messages) {
+    mod.hook('S_LIST_PARCEL_EX', 2, async event => {
+        if (!gatheringParcels) return;
 
-            if (message.items.length > 0) {
-                let attached = []
+        for (const message of event.messages) {
+            const attached = message.items.map(item => item.id);
 
-                for (let item of message.items) {
-                    attached.push(item.id)
-                }
-                parcels.push({
-                    id: message.id,
-                    status: message.status,
-                    sender: message.sender,
-                    items: attached
-                })
-                objParcels[message.id] = {
-                    id: message.id,
-                    status: message.status
-                }
-            } else {
-                parcels.push({
-                    id: message.id,
-                    status: message.status,
-                    sender: message.sender,
-                    items: []
-                })
-            }
+            parcels.push({
+                id: message.id,
+                status: message.status,
+                sender: message.sender,
+                items: attached,
+            });
         }
 
         if (event.currentPage < event.totalPages - 1) {
-            checkMail(event.currentPage + 1)
+            sender.checkMail(event.currentPage + 1);
+        } else {
+            gatheringParcels = false;
+            handleParcels();
         }
-        else {
-            HandleParcels()
-        }
-    })
+    });
 
-    function checkMail(pageIndex) {
-        working = true
-        mod.send('C_LIST_PARCEL', 2, {
-            unk1: 0,
-            page: pageIndex,
-            filter: 0
-        })
+    async function handleParcels() {
+        if (contractId !== -1) await sender.cancelContract(contractId);
+
+        await sender.requestContract();
+
+        const emptyParcels = [];
+        for (const parcel of parcels) {
+            if (parcel.status === 2 || (parcel.status === 0 && !parcel.items.length)) {
+                emptyParcels.push({id: parcel.id});
+            } else if (parcel.items.length) {
+                await sender.claimParcelItems(parcel);
+                emptyParcels.push({id: parcel.id});
+            }
+        }
+
+        if (deleteAfter && emptyParcels.length) {
+            while (emptyParcels.length) {
+                const deletableMessages = getMessagesToDelete(emptyParcels);
+                await sender.deleteParcelMessages(contractId, deletableMessages);
+            }
+        }
+
+        if (contractId !== -1) await sender.cancelContract(contractId);
     }
 
-    async function HandleParcels() {
+    function getMessagesToDelete(emptyParcels) {
+        if (!emptyParcels.length) return;
 
-        let delet = []
-        if (contract !== -1) {
-            mod.send('C_CANCEL_CONTRACT', 1, { type: 8, id: contract })
-            await new Promise(res => mod.hookOnce('S_CANCEL_CONTRACT', 'raw', res))
-            await new Promise(res => setTimeout(res, 200))
-        }
+        if (emptyParcels.length >= 10) return emptyParcels.splice(0, 10);
 
-        mod.send('C_REQUEST_CONTRACT', 1, { type: 8 })
-
-        await new Promise(res => mod.hookOnce('S_REPLY_REQUEST_CONTRACT', 'raw', res))
-
-        for (let parcel of parcels) {
-            if (parcel.status === 2 || parcel.status === 0 && parcel.items.length === 0) {
-                delet.push({ id: parcel.id })
-            }
-            else if (parcel.items.length > 0) {
-                mod.send('C_SHOW_PARCEL_MESSAGE', 1, { id: parcel.id })
-
-                await new Promise(res => mod.hookOnce('S_PARCEL_READ_RECV_STATUS', 'raw', res))
-
-                mod.send('C_RECV_PARCEL', 1, { id: parcel.id })
-
-                await new Promise(res => mod.hookOnce('S_RECV_PARCEL', 'raw', res))
-            }
-        }
-
-        if (deleteAfter) {
-            if (delet.length > 0) {
-                while (delet.length > 0) {
-                    if (delet.length >= 10) {
-                        let delet_this = delet.splice(0, 10)
-
-                        mod.send('C_DELETE_PARCEL', 2, {
-                            id: contract,
-                            messages: delet_this
-                        })
-                    }
-                    else if (delet.length > 0 && delet.length < 10) {
-                        let delet_this = delet.splice(0, delet.length)
-
-                        mod.send('C_DELETE_PARCEL', 2, {
-                            id: contract,
-                            messages: delet_this
-                        })
-                    }
-                    await new Promise(res => mod.hookOnce('S_DELETE_PARCEL', 'raw', res))
-                    await new Promise(res => setTimeout(res, 100))
-                }
-            }
-        }
-        if (contract !== -1) {
-            mod.send('C_CANCEL_CONTRACT', 1, { type: 8, id: contract })
-            await new Promise(res => mod.hookOnce('S_CANCEL_CONTRACT', 'raw', res))
-            await new Promise(res => setTimeout(res, 200))
-        }
-        working = false
+        return emptyParcels.splice(0, emptyParcels.length);
     }
-}
+};
